cc87ca44a686d42a80f58f34403696066874de8d
Edmar Moretti <edmar.moretti@gmail.com>
2018-05-04 20:57:22 -0300

    Update php-fpm-envvar.sh

--- docker/php-fpm-envvar.sh
+++ docker/php-fpm-envvar.sh
@@ -25,8 +25,8 @@ then
     echo "env[DB_PASSWORD] = $DB_PASSWORD;" >>  /etc/php/5.6/fpm/pool.d/www.conf
 fi
 echo "" >> /etc/php/5.6/fpm/pool.d/www.conf # new line.
-echo "php_admin_flag[default_charset] = ''" >>  /etc/php/5.6/fpm/pool.d/www.conf
+echo $'php_admin_flag[default_charset] = ""' >>  /etc/php/5.6/fpm/pool.d/www.conf
 echo "" >> /etc/php/5.6/fpm/pool.d/www.conf # new line.
-echo "php_admin_flag[session.save_path] = 'tcp://127.0.0.1:11211'" >>  /etc/php/5.6/fpm/pool.d/www.conf
+echo ";php_admin_flag[session.save_path] = 'tcp://127.0.0.1:11211'" >>  /etc/php/5.6/fpm/pool.d/www.conf
 echo "" >> /etc/php/5.6/fpm/pool.d/www.conf # new line.
-echo "php_admin_flag[session.save_handler] = memcache" >>  /etc/php/5.6/fpm/pool.d/www.conf
+echo ";php_admin_flag[session.save_handler] = memcache" >>  /etc/php/5.6/fpm/pool.d/www.conf
