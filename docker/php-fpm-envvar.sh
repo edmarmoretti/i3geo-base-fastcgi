bceeb5bf6f5d4cca6120f701c3394b9fa3621000
Edmar Moretti <edmar.moretti@gmail.com>
2018-05-04 22:47:35 -0300

    Update php-fpm-envvar.sh

--- docker/php-fpm-envvar.sh
+++ docker/php-fpm-envvar.sh
@@ -30,9 +30,23 @@ echo "" >> /etc/php/5.6/fpm/pool.d/www.conf # new line.
 echo ";php_admin_flag[session.save_path] = 'tcp://127.0.0.1:11211'" >>  /etc/php/5.6/fpm/pool.d/www.conf
 echo "" >> /etc/php/5.6/fpm/pool.d/www.conf # new line.
 echo ";php_admin_flag[session.save_handler] = memcache" >>  /etc/php/5.6/fpm/pool.d/www.conf
+
 echo "" >> /etc/php/5.6/fpm/pool.d/www.conf # new line.
-echo "pm = static" >>  /etc/php/5.6/fpm/pool.d/www.conf
+echo "pm =  dynamic" >>  /etc/php/5.6/fpm/pool.d/www.conf
 echo "" >> /etc/php/5.6/fpm/pool.d/www.conf # new line.
-echo "pm = static" >>  /etc/php/5.6/fpm/pool.d/www.conf
+echo "pm.max_children = 120" >>  /etc/php/5.6/fpm/pool.d/www.conf
 echo "" >> /etc/php/5.6/fpm/pool.d/www.conf # new line.
-echo "pm.max_children = 100" >>  /etc/php/5.6/fpm/pool.d/www.conf
+echo "pm.start_servers = 36" >>  /etc/php/5.6/fpm/pool.d/www.conf
+echo "" >> /etc/php/5.6/fpm/pool.d/www.conf # new line.
+echo "pm.min_spare_servers = 15" >>  /etc/php/5.6/fpm/pool.d/www.conf
+echo "" >> /etc/php/5.6/fpm/pool.d/www.conf # new line.
+echo "pm.max_spare_servers = 50" >>  /etc/php/5.6/fpm/pool.d/www.conf
+echo "" >> /etc/php/5.6/fpm/pool.d/www.conf # new line.
+echo "pm.max_requests = 200" >>  /etc/php/5.6/fpm/pool.d/www.conf
+echo "" >> /etc/php/5.6/fpm/pool.d/www.conf # new line.
+echo "pm.process_idle_timeout = 5s" >>  /etc/php/5.6/fpm/pool.d/www.conf
+
+
+
+
+
